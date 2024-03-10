8a7e55f46be06784225359428bc1074d3b79b5d2
Vazahat Shaikh <shaikhvajahat19@gmail.com>
2024-03-10 17:29:41 +0530

    Added port number in maints from env

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
