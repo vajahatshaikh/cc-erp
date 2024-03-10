9aaff21d72e0e2031dba0902b8e635618a2cd6e3
Vazahat Shaikh <shaikhvajahat19@gmail.com>
2024-03-10 17:49:05 +0530

    Reverted maints file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import process from "process";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT, '0.0.0.0');
+  await app.listen(3000);
 }
 bootstrap();
