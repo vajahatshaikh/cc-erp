65070853ada3e6c042218b7daad0c379216f2f8a
Vazahat Shaikh <shaikhvajahat19@gmail.com>
2024-04-11 01:26:23 +0530

    Added test.html file with route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import path, { join } from 'path';
+import { Response } from 'express';
+import * as fs from 'fs'; // Import fs module directly
 
 @Controller()
 export class AppController {
@@ -8,5 +11,31 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('file/:fileName')
+  async getFile(@Param('fileName') fileName: string, @Res() res: Response) {
+    const filePath = join(
+      join(__dirname, '..'),
+      '..',
+      'src',
+      'templates',
+      fileName,
+    ); // Adjust path as needed
+    try {
+      await new Promise((resolve, reject) => {
+        fs.access(filePath, fs.constants.F_OK, (err) => {
+          if (err) {
+            reject(new Error(`File not found: ${filePath}`));
+          } else {
+            resolve(true);
+          }
+        });
+      });
+      return res.sendFile(filePath);
+    } catch (error) {
+      console.error('Error fetching file:', error);
+      return res.status(404).send('File not found'); // Handle errors gracefully
+    }
   }
 }
